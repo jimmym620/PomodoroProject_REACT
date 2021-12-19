0eca723fb783c3692784bdbcf50303efeeb6fe1b
jimmym620 <jimmym620@gmail.com>
2021-12-19 12:42:40 +0000

    dnd, task list example

--- pomodoro-project_react/src/TaskList.js
+++ pomodoro-project_react/src/TaskList.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const TaskList = () => {
+    const Tasks = [
+        {
+            id: 1,
+            name: "Do homework",
+        },
+        {
+            id: 2,
+            name: "Study for physics test",
+        },
+        {
+            id: 3,
+            name: "Practice guitar",
+        },
+    ];
+
     return (
-        <section className="TaskList main-section">
+        <section className="TaskSection main-section">
             <h1>Task List component</h1>
 
-            <button>Test</button>
-            <button>Test</button>
+            <ul className="TaskList">
+                {Tasks.map(({ id, name }) => {
+                    return (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    );
+                })}
+            </ul>
         </section>
     );
 };
